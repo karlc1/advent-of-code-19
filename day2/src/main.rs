88e7eeedee2eb56f6e7fc914a82452e069eb53c1
Karl Casserfelt <karl.casserfelt@axis.com>
2019-12-02 10:11:09 +0100

    add day 2

---
+++ day2/src/main.rs
@@ -0,0 +1,40 @@
+use std::env;
+use std::fs;
+
+fn main() -> Result<(), ()> {
+    let args: Vec<String> = env::args().collect();
+
+    let day_arg = &args[1];
+    let part_arg = &args[2];
+
+    let input = fs::read_to_string("./input/input.txt")
+        .expect("Something went wrong reading the input file");
+
+    if part_arg == "1" {
+        println!("Running day {} part 1...", day_arg);
+        part1(&input)?;
+    }
+
+    if part_arg == "2" {
+        println!("Running day {} part 2...", day_arg);
+        part2(&input)?;
+    }
+
+    Ok(())
+}
+
+fn part1(input: &str) -> Result<(), ()> {
+    for line in input.lines() {
+	// Implement solution for part 1 here <------------------
+        let _remove_me = line; // remove this
+    }
+    Ok(())
+}
+
+fn part2(input: &str) -> Result<(), ()> {
+    for line in input.lines() {
+	// Implement solution for part 2 here <------------------
+        let _remove_me = line; // remove this
+    }
+    Ok(())
+}
